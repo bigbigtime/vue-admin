b18bf5b7e363fe5d20d28ae23a26f03737799d0a
bigbigtime <409019683@qq.com>
2020-02-14 21:35:01 +0800

    更新node-sass

--- vue.config.js
+++ vue.config.js
@@ -41,11 +41,12 @@ module.exports = {
     // css预设器配置项
     loaderOptions: {
         sass: { 
-            data: `@import "./src/styles/main.scss";`
+          prependData: `@import "./src/styles/main.scss";`
         }
     },
+    requireModuleExtension: true
     // 启用 CSS modules for all css / pre-processor files.
-    modules: false
+    // modules: false
   },
   // use thread-loader for babel & TS in production build
   // enabled by default if the machine has more than 1 cores
